5e5c428206bd2b7ffa7f02eb782db885b867564f
kamillediane <kdab.reyes@gmail.com>
2024-04-07 15:31:43 +0800

    Added view and verify test scripts for Deposit, Transfer and Withdraw Transaction (removed unused import statement)

--- src/test/java/cloudBankPages/WithdrawTransactionPage.java
+++ src/test/java/cloudBankPages/WithdrawTransactionPage.java
@@ -2,7 +2,6 @@ package cloudBankPages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.bidi.log.Log;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
